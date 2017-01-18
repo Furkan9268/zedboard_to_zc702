8ba255a0841848b7f0c7a9d9ff588ed20cd10046
Furkan <furkan@git.de>
2017-01-18 11:04:59 +0100

    Initial Commit

---
+++ zed_to_zc702.sdk/pwr_read/Debug/src/helloworld.d
@@ -0,0 +1,43 @@
+src/helloworld.d: ../src/helloworld.c \
+ ../../pwr_read_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../pwr_read_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../pwr_read_bsp/ps7_cortexa9_0/include/xiicps.h \
+ ../../pwr_read_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../pwr_read_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../pwr_read_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../pwr_read_bsp/ps7_cortexa9_0/include/xiicps_hw.h \
+ ../../pwr_read_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../pwr_read_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../pwr_read_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../pwr_read_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../pwr_read_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../pwr_read_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../pwr_read_bsp/ps7_cortexa9_0/include/xil_printf.h
+
+../../pwr_read_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../pwr_read_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../pwr_read_bsp/ps7_cortexa9_0/include/xiicps.h:
+
+../../pwr_read_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../pwr_read_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../pwr_read_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../pwr_read_bsp/ps7_cortexa9_0/include/xiicps_hw.h:
+
+../../pwr_read_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../pwr_read_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../pwr_read_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../pwr_read_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../pwr_read_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../pwr_read_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../pwr_read_bsp/ps7_cortexa9_0/include/xil_printf.h:
---
+++ zed_to_zc702.sdk/pwr_read/Debug/src/platform.d
@@ -0,0 +1,16 @@
+src/platform.d: ../src/platform.c \
+ ../../pwr_read_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../pwr_read_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../pwr_read_bsp/ps7_cortexa9_0/include/xil_cache.h \
+ ../../pwr_read_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../src/platform_config.h
+
+../../pwr_read_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../pwr_read_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../pwr_read_bsp/ps7_cortexa9_0/include/xil_cache.h:
+
+../../pwr_read_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../src/platform_config.h:
